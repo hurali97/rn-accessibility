192c470fea05a5a88435604b61d94e7b05196d64
hurali97@gmail.com <hurali97@gmail.com>
2020-11-08 22:58:58 +0500

    committing fonts and images

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+const allImages = {
+    images:{
+        bearwhostared: require('./bearwhostared.jpg'),
+        rating: require('./rating.png'),
+        cart: require('./cart.png'),
+        heart: require('./heart.png')
+    }
+};
+
+
+export default allImages;
